e7242556ce2be5415da7e375440f55011f5e7d21
Justin Duong <jduong@umich.edu>
2024-06-26 15:45:49 -0400

    Added simple Header Component to layout.js in /app. Removed next.js css and moved css resets global.css

---
+++ components/header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Thirteen</h1>
+      <nav>
+        <ul>
+          <li><button>Rules</button></li>
+          <li><button>Dark Mode</button></li>
+        </ul>
+      </nav>
+    </header>
+  )
+};
+
+export default Header;
