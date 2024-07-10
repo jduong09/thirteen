5599aa717ba42d7cd19c18d7d086d05407295ff9
Justin Duong <jduong@umich.edu>
2024-07-10 19:18:56 -0400

    Added logic to remove 1 card.

--- components/gameComponents/hand.js
+++ components/gameComponents/hand.js
@@ -2,13 +2,25 @@ import { useState } from 'react';
 import styles from "@/app/page.module.css";
 import { convertedHand } from '@/components/utilities/card';
 
+// Prop
+// Need to check if allowed to shed a card or not.
 export const Hand = () => {
   const [hand, setHand] = useState(convertedHand);
+
+  // Remove 1 card.
+  const removeCard = (removedCard, e) => {
+    e.preventDefault();
+    const newHand = hand.filter((card) => (card.suit != removedCard.suit || card.rank != removedCard.rank) ? true : false);
+    setHand(newHand);
+  }
+
+  // Need logic to remove combos.
+  // State to accumulate users turn.
 
   const listOfCards = hand.map((card, idx) => {
     return (
       <li key={idx}>
-        <div className={styles.card}>
+        <div className={styles.card} onClick={(e) => removeCard(card, e)}>
           <div className={styles.cardTopLeft}>
             <span>{card.rank}</span>
             <span>{card.suit}</span>
--- components/utilities/card.js
+++ components/utilities/card.js
@@ -9,7 +9,7 @@ export const makeCard = (suit, rank) => {
 
 
 /* For Testing Hand Component */
-const hand = ['A♥', '4♥', 'K♥', '5♥', '3♠', 'Q♠', '8♠', '4♦', 'J♦', '10♦', '3♦', '4♦', '5♦'];
+const hand = ['A♥', '4♥', 'K♥', '5♥', '3♠', 'Q♠', '8♠', '4♦', 'J♦', '10♦', '3♦', 'K♦', '5♦'];
 
 export const convertedHand = hand.map((str) => {
   const suit = str.slice(-1);
