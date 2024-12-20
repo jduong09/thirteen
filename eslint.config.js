7812d8f1c950c8c4ea35d165c655fd8065307822
Justin Duong <jduong@umich.edu>
2024-12-20 08:36:02 -0500

    Installed Eslint.

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginReact.configs.flat.recommended,
+  {
+      rules: {
+        'react/no-unescaped-entities': 'warn',
+        'react-hooks/rules-of-hooks': 'warn',
+        'react-hooks/exhaustive-deps': 'warn',
+      }
+  }
+];
