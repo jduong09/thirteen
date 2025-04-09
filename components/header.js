b24ca5624ab9bfbab17b5d1646dd5aba9a7a976e
Justin Duong <jduong@umich.edu>
2025-04-08 20:04:33 -0400

    DRYed up onClick functions.

--- components/header.js
+++ components/header.js
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 import React, { useState } from 'react';
 import pageStyles from '../app/page.module.css';
 import { icons } from '@/components/utilities/card';
@@ -6,32 +6,11 @@ import { icons } from '@/components/utilities/card';
 const Header = () => {
   const [headerRules, setHeaderRules] = useState({ cardRank: false, suitRank: false, combinations: false, smashes: false });
 
-  const handleCardRankClick = () => {
+  const handleClick = (type) => {
     setHeaderRules({
       ...headerRules,
-      cardRank: !headerRules.cardRank,
+      [type]: !headerRules[type],
     });
-  }
-
-  const handleSuitRankClick = () => {
-    setHeaderRules({
-      ...headerRules,
-      suitRank: !headerRules.suitRank,
-    });
-  }
-
-  const handleCombinationsClick = () => {
-    setHeaderRules({
-      ...headerRules,
-      combinations: !headerRules.combinations,
-    })
-  }
-
-  const handleSmashClick = () => {
-    setHeaderRules({
-      ...headerRules,
-      smashes: !headerRules.smashes,
-    })
   }
 
   return (
@@ -45,30 +24,30 @@ const Header = () => {
       </nav>
       <div className={pageStyles.rules}>
         <div className={pageStyles.cardRanks}>
-          <span>
+          <div className={pageStyles.navHeaders}>
             <h2>Card Ranks (High - Low)</h2>
-            <button className={headerRules.cardRank ? `${pageStyles.buttonCaret} ${pageStyles.divOpen}` : pageStyles.buttonCaret} onClick={handleCardRankClick}>
+            <button className={headerRules.cardRank ? `${pageStyles.buttonCaret} ${pageStyles.divOpen}` : pageStyles.buttonCaret} onClick={e => handleClick('cardRank')}>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>
             </button>
-          </span>
+          </div>
           <div className={headerRules.cardRank ? pageStyles.listCardRank : pageStyles.hide}>2 A K Q J 10 9 8 7 6 5 4 3 2 1</div>
         </div>
         <div className={pageStyles.suitRanks}>
-          <span>
+          <div className={pageStyles.navHeaders}>
             <h2>Suit Ranks (High - Low)</h2>
-            <button className={headerRules.suitRank ? `${pageStyles.buttonCaret} ${pageStyles.divOpen}` : pageStyles.buttonCaret} onClick={handleSuitRankClick}>
+            <button className={headerRules.suitRank ? `${pageStyles.buttonCaret} ${pageStyles.divOpen}` : pageStyles.buttonCaret} onClick={e => handleClick('suitRank')}>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>
             </button>
-          </span>
+          </div>
           <div className={headerRules.suitRank ? pageStyles.listCardRank : pageStyles.hide}>Hearts - Diamonds - Clubs - Spades</div>
         </div>
         <div className={pageStyles.ruleCombinations}>
-          <span>
+          <div className={pageStyles.navHeaders}>
             <h2>Valid Combinations</h2>
-            <button className={headerRules.combinations ? `${pageStyles.buttonCaret} ${pageStyles.divOpen}` : pageStyles.buttonCaret} onClick={handleCombinationsClick}>
+            <button className={headerRules.combinations ? `${pageStyles.buttonCaret} ${pageStyles.divOpen}` : pageStyles.buttonCaret} onClick={e => handleClick('combinations')}>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>
             </button>
-          </span>
+          </div>
           <div className={headerRules.combinations ? pageStyles.listCardRank : pageStyles.hide}>
             <div>Lead player will play valid combination, and subsequent players must follow combination or pass.</div>
             <ul className={pageStyles.listCombinations}>
@@ -130,16 +109,16 @@ const Header = () => {
                 </div>
               </li>
             </ul>
-            <div>Note: To beat someones combination, the highest card in challenger's combination must beat the previous played highest card.</div>
+            <div>Note: To beat someone's combination, the highest card in challenger's combination must beat the previous played highest card.</div>
           </div>
         </div>
         <div className={pageStyles.smashes}>
-          <span>
+          <div className={pageStyles.navHeaders}>
             <h2>Smashes</h2>
-            <button className={headerRules.smashes ? `${pageStyles.buttonCaret} ${pageStyles.divOpen}` : pageStyles.buttonCaret} onClick={handleSmashClick}>
+            <button className={headerRules.smashes ? `${pageStyles.buttonCaret} ${pageStyles.divOpen}` : pageStyles.buttonCaret} onClick={e => handleClick('smashes')}>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>
             </button>
-          </span>
+          </div>
           <div className={headerRules.smashes ? pageStyles.listCardRank : pageStyles.hide}>hello</div>
         </div>
       </div>
